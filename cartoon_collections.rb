9f0e6a38f2927508203a2d07dbb5af8cc61ee910
Daniel Gould <dmgould310@gmail.com>
2020-08-03 18:29:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,40 +3,12 @@ require 'pry'
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def roll_call_dwarves(names)
-  count = 1 
-  names.map do |name|
-    puts "#{count}. #{name}"
-    count +=1 
-    binding.pry
-  end 
-end 
-
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |call|
-    "#{call.capitalize}!"
-  end 
-end 
-
-def long_planeteer_calls(calls)
-  calls.any? do |call|
-  call.length >4
-  end 
-end 
-
-# def find_the_cheese(array)
-# cheese = ["cheddar","gouda","camembert"]
- # array.find do |type|
-  #  cheese.include?(type)
- # end 
-# end 
-
-
-def find_the_cheese(snacks)
-  cheese = ["cheddar","gouda","camembert"]
-  snacks.find do |type|
-    cheese.include?(type)
-  end 
+def roll_call_dwarves(array)
+   count = 1 
+   array.each do |names|
+     puts "#{count} #{names}"
+     count +=1 
+   end 
 end 
 
 
